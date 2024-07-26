945377ab4f3aa2045c87be3c903d85a86986b9b6
Dmitriy Syomin <syod@crtweb.ru>
2024-07-26 23:47:42 +0200

    env fixed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()],
-// 	// base: import.meta.env.DEV ? '/' : '/VRNas/',
-// })
-
 export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
