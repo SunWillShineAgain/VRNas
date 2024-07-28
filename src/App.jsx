a4e2e37ba9ecad3e44b61ca802afb4aa928f3bb1
Dmitriy Syomin <syod@crtweb.ru>
2024-07-28 18:39:13 +0200

    routing fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
-import { router } from './Router.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MainPage from './pages/MainPage.jsx'
-import AppLayout from './layout/AppLayout.jsx'
 import ServicePage from './pages/ServicePage.jsx'
 import AboutPage from './pages/AboutPage.jsx'
 import BlogPage from './pages/BlogPage.jsx'
 import Page from './pages/Page.jsx'
 
 function App() {
-	// return <RouterProvider router={router} basename='/VRNas/' />
-	console.log('VITE_URL:', import.meta.env.VITE_URL)
 	return (
 		<BrowserRouter basename={import.meta.env.VITE_URL}>
 			<Routes>
@@ -21,7 +17,6 @@ function App() {
 			</Routes>
 		</BrowserRouter>
 	)
-	// return <AppLayout />
 }
 
 export default App
